7ce2d419f1f46b2887fc33a5f5cdd6094bfc7597
shiva <98257507+Shivuduu19@users.noreply.github.com>
2025-02-21 13:20:09 +0530

    add hero component

---
+++ app/components/Hero.jsx
@@ -0,0 +1,167 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="flex items-center flex-none flex-col h-min justify-center overflow-visible pt-[120px] relative w-full">
+      <div className="flex z-[1] flex-none items-center flex-col gap-[60px] justify-center py-[40px] relative w-full">
+        <div
+          data-name="Secton Title"
+          name="Secton Title"
+          className=" place-content-center items-center flex flex-[0_0_auto] flex-col gap-[32px] h-min overflow-hidden p-0 relative w-[820px]"
+        >
+          <div
+            data-name="Title"
+            name="Title"
+            className=" place-content-center items-center justify-center flex flex-[0_0_auto]  gap-[24px] h-min overflow-hidden p-0 relative w-full"
+          >
+            <div
+              data-border="true"
+              data-name="Overlay"
+              name="Overlay"
+              className=" place-content-center items-center justify-center bg-[rgb(6,_10,_30)] rounded-[32px] [box-shadow:rgba(58,_110,_242,_0.41)_0px_2px_6px_inset] flex flex-[0_0_auto] gap-[10px] h-[15%] overflow-hidden pt-[6px] px-[16px] pb-[4px] border border-[#1c244c] relative w-min"
+            >
+              <div
+                data-component-type="RichTextContainer"
+                className=" relative flex-[0_0_auto] h-auto whitespace-pre w-auto outline-[none] flex flex-col justify-start flex-shrink-0 transform-none"
+              >
+                <p
+                  data-styles-preset="TbmJG1iM0"
+                  className=" m-0 p-0 text-white font-[300] text-[14px] leading-[1.7em] text-start"
+                >
+                  Launching soon
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            data-name="Contant"
+            name="Contant"
+            className=" place-content-center items-center justify-center flex flex-[0_0_auto] flex-col  gap-[10px] h-min overflow-hidden p-0 relative w-full"
+          >
+            <div className="contents">
+              <div
+                data-component-type="RichTextContainer"
+                className=" relative flex-[0_0_auto] h-auto whitespace-pre-wrap w-full break-words outline-[none] flex flex-col justify-start flex-shrink-0 transform-none"
+              >
+                <h1
+                  data-styles-preset="Z9AZtUOya"
+                  className=" m-0 p-0 text-[#9ea3bf] text-[64px] font-[400] tracking-[-3px] leading-[1.15em] text-center"
+                >
+                  Get Stunning Developer Portfolio in Minutes
+                </h1>
+              </div>
+            </div>
+            <div
+              data-component-type="RichTextContainer"
+              className="  flex-[0_0_auto] h-auto whitespace-pre-wrap w-[69%] break-words outline-[none] flex flex-col justify-start flex-shrink-0 transform-none"
+            >
+              <p
+                data-styles-preset="vi7hyoDN2"
+                className=" m-0 p-0 text-center text-[#9ea3bf] leading-[1.8em] text-[16px] font-normal"
+              >
+                Stand out to employers with portfolio
+              </p>
+              <p
+                data-styles-preset="vi7hyoDN2"
+                className=" text-center text-[#9ea3bf] leading-[1.8em] text-[16px] font-normal"
+              >
+                that highlights your best projects and skills
+              </p>
+              <p
+                data-styles-preset="vi7hyoDN2"
+                className="text-center text-[#9ea3bf] leading-[1.8em] text-[16px] font-normal"
+              >
+                — no design skills needed
+              </p>
+            </div>
+          </div>
+          <form className=" place-content-center items-center justify-center flex flex-[0_0_auto] [flex-flow:row] gap-[8px] h-min overflow-hidden p-0 relative w-full">
+            <div className=" flex-[3.5_0_0px] h-[60px] relative w-px overflow-hidden   bg-[rgba(187,187,187,.15)] [transition-property:background,_box-shadow] rounded-[12px]">
+              <input
+                type="email"
+                required=""
+                name="Email"
+                placeholder="jane.com"
+                defaultValue=""
+                fdprocessedid="lsc1j8"
+                className=" p-[12px_12px_12px_24px]   font-normal  text-[14px] text-[#999999] overflow-ellipsis  text-[var(--input-font-size)]  bg-transparent border-[none]  whitespace-nowrap  w-full h-full  leading-[1.2em]"
+              />
+            </div>
+            <div className=" contents">
+              <div className=" self-stretch flex-[1_0_0px] h-auto relative w-px">
+                <button
+                  type="submit"
+                  data-border="true"
+                  data-reset="button"
+                  tabIndex={0}
+                  fdprocessedid="iwzlxd"
+                  data-name="Default"
+                  className=" text-[12px] border-[rgba(158,163,191,.25)] border bg-[rgb(58,110,242)] font-[sans-serif]  p-0 bg-none place-content-center items-center cursor-pointer flex [flex-flow:row] gap-0 h-full overflow-visible justify-center relative w-full rounded-[12px] opacity-100"
+                >
+                  <div
+                    data-component-type="RichTextContainer"
+                    className=" relative flex-[0_0_auto] h-auto select-none whitespace-pre w-auto outline-[none] flex flex-col justify-start flex-shrink-0 transform-none opacity-100"
+                  >
+                    <p
+                      data-styles-preset="PdokfQujq"
+                      className=" text-[16px] leading-[1.7em] text-start font-medium text-[#9ea3bf]"
+                    >
+                      Get Access Now
+                    </p>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="flex-none h-[706px] relative w-[967px]">
+          <video
+            src="https://framerusercontent.com/assets/pC8lRLkpc2i1p1fgECgSV3JAHow.mp4"
+            loop=""
+            preload="auto"
+            muted=""
+            playsInline=""
+            style={{
+              cursor: "auto",
+              width: "100%",
+              height: "100%",
+              borderRadius: 30,
+              display: "block",
+              objectFit: "cover",
+              backgroundColor: "rgba(0, 0, 0, 0)",
+              objectPosition: "50% 50%",
+            }}
+          />
+        </div>
+      </div>
+      <div className="contents">
+        <div
+          className="aspect-[1.3333333333333333_/_1] bottom-[-400px] blur-none flex-none h-auto absolute -translate-x-2/4 w-full z-0 left-2/4
+   "
+        >
+          <div className="absolute inset-0">
+            <Image
+              decoding="async"
+              sizes="100vw"
+              width={1440}
+              height={706}
+              src="https://framerusercontent.com/images/0pkkUPiiBy68AdWhcnSLJijrCvQ.svg"
+              alt=""
+              style={{
+                display: "block",
+                width: "100%",
+                height: "100%",
+                borderRadius: "inherit",
+                objectPosition: "center center",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <header
       name="Header"
-      className=" place-content-center items-center backdrop-filter backdrop-blur bg-[rgba(5,_7,_26,_0.8)] flex flex-[0_0_auto] [flex-flow:column] gap-0 h-min left-2/4 overflow-hidden px-0 py-[30px] fixed top-0 -translate-x-1/2 w-full z-10 "
+      className=" place-content-center items-center backdrop-filter backdrop-blur bg-[rgba(5,_7,_26,_0.8)] flex flex-[0_0_auto] [flex-flow:column] gap-0 h-min left-2/4 overflow-visible p-0 md:overflow-hidden md:px-0 md:py-[30px] fixed top-0 -translate-x-1/2 w-[390px] md:w-full z-10 "
     >
       <div
         name="Container"
@@ -18,9 +18,9 @@ const Navbar = () => {
           <div className=" flex-[1_0_0px] h-auto relative w-px">
             <nav
               data-hide-scrollbars="true"
-              className=" items-center flex [flex-flow:row] h-min overflow-visible p-0 relative w-full [box-shadow:none] "
+              className=" items-center flex flex-col py-[15px] md:[flex-flow:row] h-min overflow-visible md:p-0 relative w-full [box-shadow:none] "
             >
-              <div className=" items-center flex flex-none md:flex-[1_0_0px] justify-between [flex-flow:row] md:gap-[10px] h-min overflow-visible py-5 w-full md:p-0 relative md:w-px ">
+              <div className=" items-center flex flex-none md:flex-[1_0_0px] justify-between flex-row md:gap-[10px] h-min overflow-visible px-[20px] w-full md:p-0 relative md:w-px ">
                 <div className=" flex-[0_0_auto] h-[56px] relative w-[163px] ">
                   <a
                     href="./"
@@ -73,6 +73,71 @@ const Navbar = () => {
                   </div>
                 </div>
               </div>
+              {open && (
+                <div
+                  data-framer-name="Menu"
+                  data-border="true"
+                  className="box-border [-webkit-font-smoothing:inherit] overflow-hidden items-start flex flex-[0_0_auto] flex-col bg-[#05071a] border-[#1c244c] border-[1px] gap-[8px] justify-end  h-min px-[32px] py-[24px] overflow-y-auto  absolute w-full z-10  -bottom-[150px]  left-2/4  rounded-[16px] [box-shadow:rgb(16,_22,_54)_0px_-10px_40px_0px_inset] -translate-x-1/2 opacity-100"
+                >
+                  <div className="box-border [-webkit-font-smoothing:inherit] flex-[0_0_auto] h-auto relative w-auto opacity-100">
+                    <a
+                      data-framer-name="Primary"
+                      href="./#features"
+                      className="box-border [-webkit-font-smoothing:inherit] flex items-center justify-start cursor-pointer flex-row gap-[8px] h-min overflow-visible p-0 relative no-underline w-min opacity-100"
+                    >
+                      <div
+                        data-framer-component-type="RichTextContainer"
+                        className="box-border [-webkit-font-smoothing:inherit]  flex-[0_0_auto] h-auto select-none whitespace-pre w-auto outline-[none] flex flex-col justify-start flex-shrink-0 transform-none opacity-100"
+                      >
+                        <p
+                          data-styles-preset="vi7hyoDN2"
+                          className="mobnavtext"
+                        >
+                          Benefits
+                        </p>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="box-border [-webkit-font-smoothing:inherit] flex-[0_0_auto] h-auto relative w-auto opacity-100">
+                    <a
+                      data-framer-name="Primary"
+                      href="./#how-it-works"
+                      className="box-border [-webkit-font-smoothing:inherit] flex items-center cursor-pointer [flex-flow:row] gap-[8px] h-min overflow-visible p-0 relative no-underline w-min opacity-100"
+                    >
+                      <div
+                        data-framer-component-type="RichTextContainer"
+                        className="box-border [-webkit-font-smoothing:inherit]  flex-[0_0_auto] h-auto select-none whitespace-pre w-auto outline-[none] flex flex-col justify-start flex-shrink-0 transform-none opacity-100"
+                      >
+                        <p
+                          data-styles-preset="vi7hyoDN2"
+                          className="mobnavtext"
+                        >
+                          How it works
+                        </p>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="box-border [-webkit-font-smoothing:inherit] flex-[0_0_auto] h-auto relative w-auto opacity-100">
+                    <a
+                      data-framer-name="Primary"
+                      href="./#faqs"
+                      className="box-border [-webkit-font-smoothing:inherit] flex items-center cursor-pointer [flex-flow:row] gap-[8px] h-min overflow-visible p-0 relative no-underline w-min opacity-100"
+                    >
+                      <div
+                        data-framer-component-type="RichTextContainer"
+                        className="box-border [-webkit-font-smoothing:inherit]  flex-[0_0_auto] h-auto select-none whitespace-pre w-auto outline-[none] flex flex-col justify-start flex-shrink-0 transform-none opacity-100"
+                      >
+                        <p
+                          data-styles-preset="vi7hyoDN2"
+                          className="mobnavtext"
+                        >
+                          FAQs
+                        </p>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )}
               <div className=" items-center hidden md:flex flex-[0_0_auto] [flex-flow:row] gap-[33px] h-min overflow-hidden p-0 relative w-min bg-[rgba(0,_0,_0,_0)] rounded-none z-[1] [box-shadow:none] ">
                 <div className=" linkparent ">
                   <a
@@ -105,7 +170,7 @@ const Navbar = () => {
                   </a>
                 </div>
               </div>
-              <div className=" items-center flex flex-[1_0_0px] [flex-flow:row] gap-[24px] h-min min-h-[59px] overflow-visible p-0 relative w-px " />
+              <div className=" items-center hidden md:flex flex-[1_0_0px] [flex-flow:row] gap-[24px] h-min min-h-[59px] overflow-visible p-0 relative w-px " />
             </nav>
           </div>
         </div>
